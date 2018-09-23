d8fdf34d2c31f6ba288fe09d7d0d7070d5fff265
onetoughcookie226 <onetoughcookie226@gmail.com>
2018-09-23 19:32:43 +0200

    My first component

--- scoreboard/src/App.js
+++ scoreboard/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import Title from './components/Title'
+import Title from './components/Title'
 
 class App extends Component {
   render() {
@@ -11,6 +11,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to My First React App</h1>
         </header>
+        <main>
+          <Title content="Some Simple Title" />
+        </main>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- scoreboard/src/components/Title.js
+++ scoreboard/src/components/Title.js
@@ -1,6 +1,9 @@
 import React, { PureComponent } from 'react'
- class Title extends PureComponent {
+
+export default class Title extends PureComponent {
   render() {
     return 'test'
+    return <h1>{ this.props.content }</h1>
+
   }
 }
