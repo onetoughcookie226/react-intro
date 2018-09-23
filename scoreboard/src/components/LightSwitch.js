70adfb07fe6f7bd5d2044ab14efc22dfeaa417dd
onetoughcookie226 <onetoughcookie226@gmail.com>
2018-09-23 21:44:11 +0200

    LightSwitch componant changes its own state

--- scoreboard/src/components/LightSwitch.js
+++ scoreboard/src/components/LightSwitch.js
@@ -3,9 +3,16 @@ import React, { PureComponent } from 'react'
 export default class LightSwitch extends PureComponent {
   state = { active: true }
 
+  toggle = () => {
+    this.setState({
+      active: !this.state.active
+    })
+  }
+
   render() {
     return (<div>
       <p>The light switch is <b>{ this.state.active ? 'on' : 'off' }</b></p>
+      <button onClick={this.toggle}>Toggle</button>
     </div>)
   }
 }
