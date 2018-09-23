33c5039e01e9af925d20d6ea8c3c7140cf3d64ac
onetoughcookie226 <onetoughcookie226@gmail.com>
2018-09-23 22:03:24 +0200

    Add a user componant

---
+++ scoreboard/src/components/User.js
@@ -0,0 +1,17 @@
+import React, { PureComponent } from 'react'
+
+export default class User extends PureComponent {
+render() {
+
+	// return 
+	return (<main>
+							<div>
+									<p>
+										"User: "
+										<b>Alice</b>
+									</p>
+							</div>
+					</main>
+	)
+}
+}
