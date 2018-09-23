53446536381d60ae8b892ec865c4670b82530594
onetoughcookie226 <onetoughcookie226@gmail.com>
2018-09-23 22:30:26 +0200

    Create UserBoard component

--- scoreboard/src/components/User.js
+++ scoreboard/src/components/User.js
@@ -3,15 +3,15 @@ import React, { PureComponent } from 'react'
 export default class User extends PureComponent {
 render() {
 
-	// return 
-	return (<main>
-							<div>
-									<p>
-										"User: "
-										<b>Alice</b>
-									</p>
-							</div>
-					</main>
-	)
+	return 
+	// return (<main>
+	// 						<div>
+	// 								<p>
+	// 									"User: "
+	// 									<b>Alice</b>
+	// 								</p>
+	// 						</div>
+	// 				</main>
+	// )
 }
 }
---
+++ scoreboard/src/components/UserBoard.js
@@ -0,0 +1,7 @@
+import React, { PureComponent } from 'react'
+
+export default class UserBoard extends PureComponent {
+render() {
+	return 
+    }
+}
